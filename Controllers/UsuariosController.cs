8fd1cb77f2dcaa683fc88c95232f698500f8753e
Jorge Santana <jorgersantana27@gmail.com>
2023-01-22 01:48:08 -0400

    Add project files.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Registro.Models;
+using System.Data;
+
+namespace Registro.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (SqlConnection con = new(Configuration["ConnectionStrings:conexion"]))
+            {
+                using (SqlCommand cmd = new("sp_usuarios", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataAdapter da = new(cmd);
+                    DataTable dt = new();
+                    da.Fill(dt);
+                    da.Dispose();
+                    List<UsuarioModel> lista = new();
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        lista.Add(new UsuarioModel()
+                        {
+                            id = Convert.ToInt32(dt.Rows[i][0]),
+                            nombre = (dt.Rows[i][1]).ToString(),
+                            edad = Convert.ToInt32(dt.Rows[i][2]),
+                            correo = (dt.Rows[i][3]).ToString()
+                        });
+                    }
+                    ViewBag.Usuarios = lista;
+                    con.Close();
+                }
+            }
+            return View();
+        }
+
+        public IConfiguration Configuration { get; }
+        public UsuariosController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(UsuarioModel usuario)
+        {
+            using (SqlConnection con = new(Configuration["ConnectionStrings:conexion"]))
+            {
+                using (SqlCommand cmd = new("sp_registrar", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("nombre", System.Data.SqlDbType.VarChar).Value = usuario.nombre;
+                    cmd.Parameters.Add("edad", System.Data.SqlDbType.Int).Value = usuario.edad;
+                    cmd.Parameters.Add("correo", System.Data.SqlDbType.VarChar).Value = usuario.correo;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            return Redirect("Index");
+        }
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Registro.Models
+{
+    public class UsuarioModel
+    {
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public int edad { get; set;}
+        public string? correo { get; set;}
+    }
+}
